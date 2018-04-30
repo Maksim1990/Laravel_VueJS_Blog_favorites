14a89bb6ebd89ec8e8464aaa6ccaa21ef3ad203a
Your Name <you@example.com>
2018-04-30 14:24:33 +0300

    - Initial commit - Added Favorite vue component - Added like/dislike routes - Added main style file

---
+++ app/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use App\Favorite;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'body'
+    ];
+
+    public function favorited()
+    {
+
+        return (bool) Favorite::where('user_id', Auth::id())
+            ->where('post_id', $this->id)
+            ->first();
+    }
+}
